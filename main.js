9f2d72cc089872b27abd7bb3bf16ece9339c3e06
Csontos <csontostibor48@gmail.com>
2023-11-06 17:12:42 +0100

    kész

--- main.js
+++ main.js
@@ -1,24 +1,79 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+import quotesData from './quotes.json';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  function oidezet() {
+    const idezetlista = document.getElementById('idezetl');
+    const lista = Array.from(quotesData.quotes);
+    lista.sort((a, b) => a.author.localeCompare(b.author));
+    const ul = document.createElement('ul');
+    lista.forEach(quote => {
+      const li = document.createElement('li');
+      li.innerHTML = `${quote.author} :${quote.quote}`;
+      ul.appendChild(li);
+    });
+    idezetlista.appendChild(ul);
+  }
+  
+  const btn1 = document.getElementById('btn1');
+  btn1.addEventListener('click', oidezet);
+
+
+  function the() {
+    
+    const thelista = document.getElementById('thelista');
+    const lista = Array.from(quotesData.quotes);
+    const ul = document.createElement('ol');
+  
+    lista.forEach(quote => {
+      if (quote.quote.toLowerCase().includes('the')) {
+        const li = document.createElement('li');
+        li.innerHTML = `${quote.author}: ${quote.quote.replace(/the/gi, '<strong>The</strong>')}`;
+        ul.appendChild(li);
+      }
+    });
+  
+    thelista.appendChild(ul);
+  }
+  
+  const btn2 = document.getElementById('btn2');
+  btn2.addEventListener('click', the);
+  
+  function hossz() {
+   
+
+    const hosszlista = document.getElementById('hosszlista');
+    const lista = Array.from(quotesData.quotes);
+    const lengths = lista.map(quote => quote.quote.length);
+    const lengthsString = lengths.join(', ');
+  
+    hosszlista.textContent = lengthsString;
+  }
+  
+  const btn3 = document.getElementById('btn3');
+  btn3.addEventListener('click', hossz);
+
+  function db() {
+    
+
+    const input = document.getElementById('input');
+    const szamol = document.getElementById('szamol');
+    const lista = Array.from(quotesData.quotes);
+    const szerzonev = input.value;
+    const szerzosz = lista.filter(quote => quote.author === szerzonev);
+  
+    szamol.value = szerzosz.length;
+  }
+  
+  const btn4 = document.getElementById('btn4');
+  btn4.addEventListener('click', db);
+  
+});
+
+
+
+
+
